68a5cf918b2d7bf11a25744c9225c5c4553df54f
katiewang22 <114708534+Harank-sucks-at-coding@users.noreply.github.com>
2022-11-03 19:52:08 -0400

    Haran Coding 11/3
    L code

--- src/main/java/frc/robot/subsystems/Drive test 1.java
+++ src/main/java/frc/robot/subsystems/Drive test 1.java
@@ -10,13 +10,33 @@ public class DriveSubsytem extends SubsystemBase {
   /** Creates a new DriveSubsytem. 
    * @return */
   public DriveSubsystem();
-   public void resetEncoders(){
-    backLeftMotor.setSelectedSensorPosition(0);
-    backRightMotor.setSelectedSensorPosition(0);
-    frontLeftMotor.setSelectedSensorPosition(0);
-    frontRightMotor.setSelectedSensorPosition(0);
-   }
+    void resetEncoders() 
+public void resetEncoders() {
+Object backLeftMotor;
+((Object) backLeftMotor).setSelectedSensorPosition(0);
+Object frontLeftMotor;
+((Object) frontLeftMotor).setSelectedSensorPosition(0);
+Object frontRightMotor;
+((Object)frontRightMotor).setSelectedSensorPosition(0);
+Object backRightMotor;
+backRightMotor.setSelectedSensorPosition(0);
+}
+public double getRightBackEncoderPosition(){
+  returnRightBackMotor.getSelectedSensorPosition();
+}
+public static void drive(double throttle,double rotate){
+  leftFrontMotor.set(throttle+rotate);
+  rightFrontMotor.set(throttle-rotate);
+  leftBackMotor.set(throttle+rotate);
+  rightBackMotor.set(throttle-rotate);
 
+}
+
+public double getRightBackEncoderVelocityMetersPerSecond(){
+  double leftVelocityMPS=(leftBackMotor.getselectedSensorVelocity()*10);
+  leftVelocityMPS=leftVelocityMPS*METERS_PER_TICKS;
+  return(leftVelocityMPS);
+}
   private static final WPI_TalonFX leftBackMotor= RobotMap.leftBackDriveMotor;
 private static final WPI_TalonFx rightBackMotor= RobotMap.rightBackDriveMotor;
 private static final WPI_TalonFX leftFrontMotor= RobotMap.leftFrontDriveMotor;
@@ -62,106 +82,17 @@ leftBackMotor.setNuetralMode(NuetralModeCoast);
 rightFrontMotor.setNuetralMode(NuetralModeCoast);
 rightBackMotor.setNuetralMode(NuetralModeCoast);
 
-public static void drive(double throttle, double rotate){
-  leftFrontMotor.set(throttle+rotate);
-  rightFrontMotor.set(throttle-rotate);
-  leftBackMotor.set(throttle+rotate);
-  rightBackMotor.set(throttle-rotate);
-}
-public double GetRightBackEncoderPosition(){
-  return rightBackMotor.getSelectedSensorPosition();
-}
-public double distanceTravelledinTicks(){
-  return(getLeftBackEncoderPosition()+GetRightBackEncoderPosition())/2;
-}
-public double getLeftBackEncoderVelocityMetersPerSecond(){
-  double leftVelocityMPS=(leftBackMotor.getSelectedSensorVelocity()*10);
-  leftVelocityMPS=leftVelocityMPS*METERS_PER_TICKS;
-  return(leftVelocityMPS);
-}
-public double leftDistanceTravelledInMeters(){
-  double left_dist= getLeftBackEncoderPosition()*METERS_PER_TICKS;
-  return left_dist;}
-  public void stop(){
-    drive(0,0);
-  }
-  frontLeftMotor.set(ControlMode.Follower, backLeftMotor.getDeviceID());
-  frontRightMotor.set(ControlMode.Follower, backRightMotor.getDeviceID());
-  leftMotor.setNuetralMode(NuetralMode.Coast);
-  rightMotor.setNuetralMode(NuetralMode.Coast);
-  leftMotorFront.configNominalOutputForward(0,TIMEOUT_MS);
-  leftMotorFront.configNominalOutputReverse(0,TIMEOUT_MS);
-  leftMotorFront.configPeakOutputForward(1,TIMEOUT_MS);
-  leftMotorFront.configPeakOutputReverse(-1, TIMEOUT_MS);
-  rightMotorFront.configNominalOutputForward(0,TIMEOUT_MS);
-  rightMotorFront.configNominalOutputReverse(0, TIMEOUT_MS);
-  rightMotorFront.configPeakOutputForward(1,TIMEOUT_MS);
-  rightMotorFront.configPeakOutputReverse(-1,TIMEOUT_MS);
-  leftMotorFront.configNuetralDeadband(0.001,TIMEOUT_MS);
-  leftMotorBack.configNuetralDeadband(0.001, TIMEOUT_MS);
-  rightMotorFront.configNuetralDeadband(0.001,TIMEOUT_MS);
-  rightMotorBack.configNuetralDeadband(0.001,TIMEOUT_MS);
-  frontLeftMotor.setSensorPhase(true);
-  frontRightMotor.setSensorPhase(false);
-  backLeftMotor.setSensorPhase(true);
-  backRightMotor.setSensorPhase(false);
-  frontLeftMotor.setInverted(false);
-  backRightMotor.setInverted(true);
-  backLeftMotor.setInverted(false);
-  
+leftFrontMotor.setInverted(false);
+leftBackMotor.setInverted(true);
+rightFrontMotor.setInverted(false);
+rightBackMotor.setInverted(true);
 
-  public DriveSubsytem() {
-  }
+
+public double distanceTravelledinTicks(){
+  return(getRightBackEncoderPosition()+getRightBackEncoderPosition())/2;
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-  public static WPI_TalonFX getLeftbackmotor() {
-    return leftBackMotor;
-  }
-  public static WPI_TalonFx getRightbackmotor() {
-    return rightBackMotor;
-  }
-  public static WPI_TalonFX getLeftfrontmotor() {
-    return leftFrontMotor;
-  }
-  public static WPI_TalonFX getRightfrontmotor() {
-    return rightFrontMotor;
-  }
-  public static double getInToM() {
-    return IN_TO_M;
-  }
-  public static int getMotorEncoderCodesPerRev() {
-    return MOTOR_ENCODER_CODES_PER_REV;
-  }
-  public static double getDiamterInches() {
-    return DIAMTER_INCHES;
-  }
-  public static double getWheelDiameter() {
-    return WHEEL_DIAMETER;
-  }
-  public static double getWheelCircumfrence() {
-    return WHEEL_CIRCUMFRENCE;
-  }
-  public static double getGearRatio() {
-    return GEAR_RATIO;
-  }
-  public static double getTicksPerMeter() {
-    return TICKS_PER_METER;
-  }
-  public static double getMetersPerTicks() {
-    return METERS_PER_TICKS;
-  }
-  public void getCom() {
-    return com;
-  }
-  public void setCom(void com) {
-    this.com = com;
-  }
-  public void getCom() {
-    return com;
-  }
-  public void setCom(void com) {
-    this.com = com;
   }
 }
